363499c4ef1cd4e8e18f4b1d3948ce8093b082f2
Atul Kumar <919atul@gmail.com>
2024-08-17 10:09:22 +0530

    teacher student updated

--- frontend/src/components/Auth/Login.jsx
+++ frontend/src/components/Auth/Login.jsx
@@ -55,8 +55,8 @@ const Login = () => {
               <div>
                 <select value={role} onChange={(e) => setRole(e.target.value)}>
                   <option value="">Select Role</option>
-                  <option value="Employer">Employer</option>
-                  <option value="Job Seeker">Job Seeker</option>
+                  <option value="Employer">Teacher</option>
+                  <option value="Job Seeker">Student</option>
                 </select>
                 <FaRegUser />
               </div>
