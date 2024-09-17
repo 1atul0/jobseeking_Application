b62613c631e29d15237b7d6309148b6ebd897ed6
Atul Kumar <919atul@gmail.com>
2024-09-17 16:02:14 +0530

    updatd

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ import jobRouter from "./routes/jobRouter.js";
 import { dbConnection } from './database/dbConnection.js';
 import { errorMiddleware } from './middlewares/error.js';
 import cloudinary from "cloudinary";
-// import path from "path";
+import path from "path";
 
 
 //starting express app
@@ -20,7 +20,7 @@ cloudinary.v2.config({
     api_secret: process.env.CLOUDINARY_SECRET_KEY
 });
 
-// const _dirname = path.resolve();
+const _dirname = path.resolve();
 
 
 
@@ -59,22 +59,22 @@ dbConnection();
 //setting error middleware
 app.use(errorMiddleware)
 // Serve static frontend assets in production
-if (process.env.NODE_ENV === 'production') {
-    const path = require('path');
+// if (process.env.NODE_ENV === 'production') {
+//     const path = require('path');
     
-    // Serve the frontend build
-    app.use(express.static(path.join(__dirname, 'frontend', 'build')));
+//     // Serve the frontend build
+//     app.use(express.static(path.join(__dirname, 'frontend', 'build')));
   
-    // Serve the index.html for any other route
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-    });
-  }
-// app.use(express.static(path.join(_dirname,"/frontend/dist")))
-// app.get("*",(req,res)=>{
-//     // res.send("<H1>Page not found.</H1>");
-//     res.sendFile(path.resolve(_dirname,"frontend","dist","index.html"))
-// })
+//     // Serve the index.html for any other route
+//     app.get('*', (req, res) => {
+//       res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+//     });
+//   }
+app.use(express.static(path.join(_dirname,"/frontend/dist")))
+app.get("*",(req,res)=>{
+    // res.send("<H1>Page not found.</H1>");
+    res.sendFile(path.resolve(_dirname,"frontend","dist","index.html"))
+})
 
 app.listen(process.env.PORT || 3000, () => {
     console.log(`Server is running on port ${process.env.PORT || 3000}`);
